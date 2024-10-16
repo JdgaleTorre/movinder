135d24a1c1d3cc36f0cb4de934eae6338a018f72
Jose Gale <josedavidgale@gmail.com>
2024-10-16 15:48:47 +0100

    Adding discord to Images url and collectMovie Scripts

--- next.config.js
+++ next.config.js
@@ -6,22 +6,28 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'image.tmdb.org',
-            port: '',
-            // pathname: '/account123/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
-            // pathname: '/account123/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        // pathname: '/account123/**',
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        // pathname: '/account123/**',
+      },
+    ],
+  },
 };
 
 export default config;
---
+++ scripts/collectMovieVotes.ts
@@ -0,0 +1,79 @@
+import { db } from "~/server/db";
+import * as fs from "fs";
+import * as path from "path";
+
+type tempMovieVote = {
+    movieId: number,
+    movieTitle: string,
+    createdById: string,
+    createdAt: Date,
+    vote: number
+}
+
+
+const exportToCsv = (filename: string, rows: Array<tempMovieVote>, headers?: string[]): void => {
+    if (!rows?.length) {
+        return;
+    }
+    const separator = ",";
+
+    const keys: (keyof tempMovieVote)[] = Object.keys(rows[0] ?? {}) as (keyof tempMovieVote)[];
+
+    let columHearders: string[];
+
+    if (headers) {
+        columHearders = headers;
+    } else {
+        columHearders = keys as string[];
+    }
+
+    const csvContent =
+        columHearders.join(separator) +
+        '\n' +
+        rows.map(row => {
+            return keys.map(k => {
+                switch (k) {
+                    case 'createdAt':
+                        return row[k].toISOString().replace('T', '').replace('Z', '');
+                    case 'movieTitle':
+                        const title = row[k].toString().replace(/"/g, '""');
+                        if (title.search(',') > 1) return `"${title}"`;
+                        return title;
+                    default:
+                        return row[k].toString().replace(/"/g, '""');
+                }
+            }).join(separator);
+        }).join('\n');
+
+    const csvFilePath = path.resolve(filename);
+    fs.writeFile(csvFilePath, csvContent, 'utf8', (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
+const collectVotes = async () => {
+    const MovieVotes = await db.movieVote.findMany({
+        include: { movie: true }
+    });
+
+    const votes = new Array<tempMovieVote>();
+    //loop through movie votes and create a new array with the movieId, createdById, createdAt and vote value
+    for (const movieVote of MovieVotes) {
+        const vote = {
+            movieId: movieVote.movie.id,
+            movieTitle: movieVote.movie.title,
+            createdById: movieVote.createdById,
+            createdAt: movieVote.createdAt,
+            vote: movieVote.vote
+        }
+        votes.push(vote)
+    }
+
+    // console.log(votes)
+    exportToCsv('./MoviesVotes.csv', votes);
+}
+
+await collectVotes()
